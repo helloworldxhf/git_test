62db73328b6cf6c9023126d8370abea911bd76a7
helloworldxhf <103037355+helloworldxhf@users.noreply.github.com>
2022-04-05 22:55:16 +0800

    pull commit

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -9,5 +9,7 @@ public class TestGit {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
+        
     }
 }
